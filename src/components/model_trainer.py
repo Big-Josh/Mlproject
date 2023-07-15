73db009212fa63808832fef5377b9646521663b0
Big-Josh <otoojoshua616@gmail.com>
2023-07-14 18:25:52 -0700

    Predict

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -20,7 +20,7 @@ from src.pipeline.utils import evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join('artificats', 'model.pkl')
+    trained_model_file_path = os.path.join('artifacts', 'model.pkl')
 
 class ModelTrainer:
     def __init__(self) :
@@ -49,8 +49,50 @@ class ModelTrainer:
                 "CatBoost" : CatBoostRegressor(verbose = False),
                 "XGBoost" : XGBRegressor()
             }
-    
-            model_report:dict = evaluate_models(X_train = X_train, y_train = y_train, X_test = X_test, y_test = y_test, models = models)
+
+            params={
+                "Decision Tree": {
+                    'criterion':['friedman_mse',  'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                
+
+                "Linear Regression":{
+
+                },
+
+                "XGBoost":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+
+                "CatBoost":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+
+                "AdaBoost":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+
+                "k Neighbor": {
+                    'n_neighbors' : [3,5,6,7,8],
+                    'weights' : ['uniform', 'distance']
+                }
+                
+            }
+
+            model_report:dict = evaluate_models(X_train = X_train, y_train = y_train, X_test = X_test, y_test = y_test, models = models, param = params)
 
             #To get best model score from dict
             best_model_score  = max(sorted(model_report.values()))
